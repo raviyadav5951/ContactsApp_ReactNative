85016fb7e0d3aa8e2cdcedede6237bd627885c0e
Ravi <raviyadav5951@gmail.com>
2022-07-22 00:15:23 +0530

    initial code setup done

---
+++ src/navigations/index.js
@@ -0,0 +1,16 @@
+import {NavigationContainer} from '@react-navigation/native';
+import {Text} from 'react-native';
+import React from 'react';
+
+const AppNavigationContainer = () => {
+  return (
+    <NavigationContainer>
+      <Text>Test</Text>
+    </NavigationContainer>
+  );
+};
+
+//screens->Home->drawer
+//screens->Auth(pre login screens)
+
+export default AppNavigationContainer;
